b9043f169c53e582223b65df5b726b9e0f8ecb7a
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-12 15:46:19 -0300

    segunto test

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -7,4 +7,9 @@ describe Calculator do
     expect(result).to eq(12)
   end
 
+  it 'use sum method for 2 numbers with negative numbers' do
+    calc = Calculator.new
+    result = calc.sum(-5, 7)
+    expect(result).to eq(2)
+  end
 end
