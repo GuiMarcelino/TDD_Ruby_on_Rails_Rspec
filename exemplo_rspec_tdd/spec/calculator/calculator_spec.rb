9a8cd497ca60b9130f02d25bca4294024a60bd20
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-13 06:44:48 -0300

    refatorando e add --format documentation

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,15 +1,23 @@
 require 'Calculator'
 
 describe Calculator do
-  it 'use sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 7)
-    expect(result).to eq(12)
-  end
+  context '#sum' do
+    it 'positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
 
-  it 'use sum method for 2 numbers with negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, 7)
-    expect(result).to eq(2)
+    it 'negative and positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, 7)
+      expect(result).to eq(2)
+    end
+
+    it 'negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
   end
 end
