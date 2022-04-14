67e114992ddd2e84cf297c2ae11f3d61e8effe59
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-14 10:12:23 -0300

    usando o is_expected

--- exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -2,5 +2,6 @@ describe (1..5), 'Range' do
   it '#cover' do
     expect(subject).to cover(2,5) # espera uqe enha o 2 eo 5 no range, é a mesma coisa que o include so que com range
   end
+  it { is_expected.to cover(3,4) }  # usando is_expected
 
 end
