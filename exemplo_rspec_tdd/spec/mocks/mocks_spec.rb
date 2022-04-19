0c18c5c437e89a9bbab1c6af47dedf15f559c2e8
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-19 19:51:26 -0300

    mock expectations

--- exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
+++ exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
@@ -15,4 +15,24 @@ describe 'Mocks' do
     #exercise
     student.bar
   end
+
+  it 'arg' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  it 'repetição' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    student.foo(123)
+    student.foo(123)
+
+  end
+
+  it 'retorno' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    puts student.foo(123)
+  end
 end
